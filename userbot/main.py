a1ee85b7d64d04a7c1f44c37400782dfeae8b08d
Alex <joubert2006@hotmail.com>
2025-04-24 00:07:11 -0300

    está chegando o codigo, falta fazer o login

--- userbot/main.py
+++ userbot/main.py
@@ -9,13 +9,20 @@ api_hash = "66a174ab12f849b8c3a1fe18352d5010"
 # api_hash = "6a6cc3f9ef2c38453bf90bb63ab92d38"
 
 async def login_and_send_messages(phone, code):
-    client = TelegramClient(f"session_{phone}", api_id, api_hash)
+    client = TelegramClient(f"session_+{phone}", api_id, api_hash)
     await client.connect()
 
     if not await client.is_user_authorized():
         try:
-            await client.send_code_request(phone)
-            await client.sign_in(phone=phone, code=code)
+            print("Enviando solicitação de código para o número:", f'+{phone}')
+            sent = await client.send_code_request(f'+{phone}')
+            print(sent)
+            
+            # Aguarda o usuário inserir o código recebido
+            code = input("Digite o código recebido na tela: ")
+            while not code.strip():
+                code = input("O código não pode estar vazio. Digite novamente: ")
+            await client.sign_in(phone=f'+{phone}', code=code)
         except Exception as e:
             return f"Erro ao logar: {e}"
 
